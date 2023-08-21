f3d0111f02222ac8b4376123e80021944f42995a
Akash <akashikur011@gmail.com>
2023-08-21 19:47:55 +0530

    first-1

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import Tabs from "./Tabs";
 
 const App = () => {
   const tabs1 = [
-    { title: "Tab 1", content: "Content of Tab 1" },
-    { title: "Tab 2", content: "Content of Tab 2" },
-    { title: "Tab 3", content: "Content of Tab 3" },
+    { title: "Tab 1", content: "Content for Tab 1" },
+    { title: "Tab 2", content: "Content for Tab 2" },
+    { title: "Tab 3", content: "Content for Tab 3" },
   ];
 
   const tabs2 = [
-    { title: "Tab A", content: "Content of Tab A" },
-    { title: "Tab B", content: "Content of Tab B" },
-    { title: "Tab C", content: "Content of Tab C" },
+    { title: "Tab A", content: "Content for Tab A" },
+    { title: "Tab B", content: "Content for Tab B" },
+    { title: "Tab C", content: "Content for Tab C" },
   ];
 
   return (
